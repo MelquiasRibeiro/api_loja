0c9bda8d2b33b201c1fad222f0131f02307a2d65
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-10-05 20:00:36 -0300

    feat:

--- src/main/java/com/melquias/lojaapi/LojaapiApplication.java
+++ src/main/java/com/melquias/lojaapi/LojaapiApplication.java
@@ -1,13 +1,32 @@
 package com.melquias.lojaapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LojaapiApplication {
+import com.melquias.lojaapi.domain.Categoria;
+import com.melquias.lojaapi.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class LojaapiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaapiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");	
+		Categoria cat2 = new Categoria(null,"Escritório");		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+	}
+
+	
 }
