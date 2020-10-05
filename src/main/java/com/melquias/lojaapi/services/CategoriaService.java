a7f503447d6d1187d1f80f0637b9bdf6d8a2be33
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-10-05 19:43:03 -0300

    feat: creat repository class, resouce class and  show category endPoint

---
+++ src/main/java/com/melquias/lojaapi/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.melquias.lojaapi.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.melquias.lojaapi.domain.Categoria;
+import com.melquias.lojaapi.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null); 
+	}
+	
+}
