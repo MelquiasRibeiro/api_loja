37d97a4549fe85b5bf8a6ef88261de08e8b2208e
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-10-05 16:20:50 -0300

    feat: initial commit :leaves:

---
+++ src/main/java/com/melquias/lojaapi/LojaapiApplication.java
@@ -0,0 +1,13 @@
+package com.melquias.lojaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaapiApplication.class, args);
+	}
+
+}
