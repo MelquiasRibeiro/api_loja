37e2d933be82069bf1d1b7ff3845a7b5896c6600
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-10-07 17:27:06 -0300

    feat:categoria pagination

--- src/main/java/com/melquias/lojaapi/services/CategoriaService.java
+++ src/main/java/com/melquias/lojaapi/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.melquias.lojaapi.domain.Categoria;
@@ -22,7 +25,7 @@ public class CategoriaService {
 		 Optional<Categoria> obj = repo.findById(id);
 			return obj.orElseThrow(() -> new ObjectNotFoundException(
 			 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-			}
+	}
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
@@ -33,6 +36,7 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+	
 	public void delete(Integer id) {	
 		find(id);
 		try {			
@@ -41,7 +45,16 @@ public class CategoriaService {
 			throw new DataIntegrityException("impossivel exlcuir uma categoria que possui produtos");	
 		}
 	}
+	
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		
+		return repo.findAll(pageRequest);	
+	}
+	
 }
