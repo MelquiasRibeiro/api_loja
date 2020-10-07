dbd8fea3b73a432935d6a210e43361cbca907047
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-10-07 09:51:42 -0300

    feat: store endPoint to categorias

--- src/main/java/com/melquias/lojaapi/LojaapiApplication.java
+++ src/main/java/com/melquias/lojaapi/LojaapiApplication.java
@@ -75,12 +75,12 @@ public class LojaapiApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null,"impressora",1000.00);		
 		Produto p3 = new Produto(null,"mouse",80.00);
 		
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2,p3));
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
-		p2.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
 		
--- src/main/java/com/melquias/lojaapi/resources/CategoriaResource.java
+++ src/main/java/com/melquias/lojaapi/resources/CategoriaResource.java
@@ -1,12 +1,16 @@
 package com.melquias.lojaapi.resources;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.melquias.lojaapi.domain.Categoria;
 import com.melquias.lojaapi.services.CategoriaService;
@@ -25,6 +29,16 @@ public class CategoriaResource {
 		Categoria obj= service.buscar(id);
 	
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> create(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path( "/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
 	}
 	
 }
